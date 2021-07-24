defa972845f0284f5bfae750aa87a3d93323bcab
Łukasz Cwyl <lukasz.cwyl@commart.pl>
2021-07-24 22:18:55 +0200

    Zwróć większą liczbę

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let num1 = 2;
+let num2 = 5;
+    
+function zwrocWiekszaLiczbe(x = num1 ,y = num2){ 
+    
+ if(x > y){
+    return num1;
+ }
+ else{
+    return num2;
+ }
+    
+}
+console.log(zwrocWiekszaLiczbe());
+
+
+
